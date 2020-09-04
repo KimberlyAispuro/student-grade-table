87ffed8c0507cf881d50a7272f094cd4b02589dc
Kimberly Aispuro <kimberlylex@gmail.com>
2020-09-04 22:40:28 +0000

    Need help, won't render after completing ALL steps prior to DELETE a grade

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,6 +1,7 @@
 class GradeTable{
-    constructor(tableElement){
+    constructor(tableElement,noGradesElement){
         this.tableElement = tableElement;
+        this.noGradesElement = noGradesElement;
     }
     updateGrades(grades){
         console.log(grades);
@@ -11,19 +12,68 @@ class GradeTable{
         for(var i=0; i<grades.length; i++){
             var gradesIndex = grades[i];
 
+            this.renderGradeRow(gradesIndex,this.deleteGrade);
+
+            if(grades.length === 0){
+                var pEl = document.getElementById('no-grades');
+                pEl.classList.remove("d-none");
+            }
+
+            // tableBodyEl.appendChild(renderRow);
+
+            // var tableRow = document.createElement('tr');
+
+            // var studentName = document.createElement('td');
+            // studentName.textContent = gradesIndex.name;
+            // tableRow.appendChild(studentName);
+
+            // var studentCourse = document.createElement('td');
+            // studentCourse.textContent = gradesIndex.course;
+            // tableRow.appendChild(studentCourse);
+
+            // var studentGrade = document.createElement('td');
+            // studentGrade.textContent = gradesIndex.grade;
+            // tableRow.appendChild(studentGrade);
+
+            // tableBodyEl.appendChild(tableRow);
+        }
+    }
+    onDeleteClick(deleteGrade){
+        this.deleteGrade = deleteGrade;
+
+    }
+    renderGradeRow(data,deleteGrade){
+        var tableBodyEl = this.tableElement.querySelector('tbody');
+        // tableBodyEl.textContent = " ";
+
+        for(var i=0; i<data.length; i++){
+            var dataIndex = data[i];
+
             var tableRow = document.createElement('tr');
 
             var studentName = document.createElement('td');
-            studentName.textContent = gradesIndex.name;
+            studentName.textContent = dataIndex.name;
             tableRow.appendChild(studentName);
 
             var studentCourse = document.createElement('td');
-            studentCourse.textContent = gradesIndex.course;
+            studentCourse.textContent = dataIndex.course;
             tableRow.appendChild(studentCourse);
 
             var studentGrade = document.createElement('td');
-            studentGrade.textContent = gradesIndex.grade;
+            studentGrade.textContent = dataIndex.grade;
             tableRow.appendChild(studentGrade);
+
+            var operationsCol = document.createElement('td');
+            tableRow.appendChild(operationsCol);
+
+            var createButton = document.createElement('button');
+            createButton.classList = 'btn btn-danger';
+            createButton.textContent = 'delete';
+            operationsCol.appendChild(createButton);
+            // createButton.addEventListener('click', deleteGrade(dataIndex.id));
+            createButton.addEventListener('click', function(){
+                deleteGrade(dataIndex.id);
+            });
 
             tableBodyEl.appendChild(tableRow);
         }
--- main.js
+++ main.js
@@ -4,11 +4,13 @@ var headerEl = document.querySelector('header');
 
 var tableEl = document.querySelector('table');
 
+var pElement = document.querySelector('p');
+
 var gradeForm = new GradeForm(formEl);
 
 var pageHeader = new PageHeader(headerEl);
 
-var gradeTable = new GradeTable(tableEl);
+var gradeTable = new GradeTable(tableEl,pElement);
 
 var loadApp = new App(gradeTable,pageHeader,gradeForm);
 loadApp.start();
